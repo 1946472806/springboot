10e41a0857a50fb327b1cdf316b4a963e88b429c
1946472806 <1946472806@qq.com>
2019-08-09 00:14:13 +0800

    集成jsp

---
+++ chapter01/src/main/java/org/sang/controller/JspController.java
@@ -0,0 +1,12 @@
+package org.sang.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JspController {
+    @GetMapping("/jsp")
+    public String jspTest(){
+        return "my";
+    }
+}
